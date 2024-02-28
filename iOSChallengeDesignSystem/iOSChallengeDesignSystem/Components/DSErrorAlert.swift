2ab4d5e96d4e2ec71e90a112fd02c2ee58883c59
Fernando Bustos <lufe.bura@gmail.com>
2024-02-28 12:31:46 -0600

    feat: background added in DSErrorAlert
    Alpha background added to DSErrorAlert to limit user interaction

--- iOSChallengeDesignSystem/iOSChallengeDesignSystem/Components/DSErrorAlert.swift
+++ iOSChallengeDesignSystem/iOSChallengeDesignSystem/Components/DSErrorAlert.swift
@@ -17,47 +17,52 @@ public struct DSErrorAlert: View {
     }
     
     public var body: some View {
-        VStack {
-            Image(systemName: "exclamationmark.triangle.fill")
-                .resizable()
-                .frame(width: DSSize.bigger.floatValue, height: DSSize.bigger.floatValue)
-                .foregroundStyle(DSColor.neutral)
-                .padding()
-            Text(error.title)
-                .lineLimit(2)
-                .multilineTextAlignment(.center)
-                .font(Typography.boldX.font)
-                .foregroundStyle(DSColor.neutral)
-            Text(error.detail)
-                .lineLimit(8)
-                .multilineTextAlignment(.center)
-                .font(Typography.regularX.font)
-                .foregroundStyle(DSColor.neutral)
-                .padding(.horizontal)
-                .padding(.bottom)
-                .padding(.top, DSSize.minimum.floatValue)
-            DSButton(
-                style: .mainly,
-                icon: nil,
-                message: error.firstAction.title,
-                action: error.firstAction.action
-            )
-            .padding(.horizontal)
-            .padding(.bottom)
-            if let action = error.secondAction {
+        ZStack {
+            DSColor.tertiary
+                .edgesIgnoringSafeArea(.all)
+                .opacity(0.5)
+            VStack {
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .resizable()
+                    .frame(width: DSSize.bigger.floatValue, height: DSSize.bigger.floatValue)
+                    .foregroundStyle(DSColor.neutral)
+                    .padding()
+                Text(error.title)
+                    .lineLimit(2)
+                    .multilineTextAlignment(.center)
+                    .font(Typography.boldX.font)
+                    .foregroundStyle(DSColor.neutral)
+                Text(error.detail)
+                    .lineLimit(8)
+                    .multilineTextAlignment(.center)
+                    .font(Typography.regularX.font)
+                    .foregroundStyle(DSColor.neutral)
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                    .padding(.top, DSSize.minimum.floatValue)
                 DSButton(
-                    style: .secondary,
+                    style: .mainly,
                     icon: nil,
-                    message: action.title,
-                    action: action.action
+                    message: error.firstAction.title,
+                    action: error.firstAction.action
                 )
                 .padding(.horizontal)
                 .padding(.bottom)
+                if let action = error.secondAction {
+                    DSButton(
+                        style: .secondary,
+                        icon: nil,
+                        message: action.title,
+                        action: action.action
+                    )
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                }
             }
+            .background(DSColor.secondary)
+            .cornerRadius(DSSize.normal.floatValue)
+            .padding()
         }
-        .background(DSColor.secondary)
-        .cornerRadius(DSSize.normal.floatValue)
-        .padding()
     }
 }
 
